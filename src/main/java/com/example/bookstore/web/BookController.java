c1a1ca5997ac033aa78a34d4c5d99021830ae3d1
Thu Trang Vu <vuthutrang2811@gmail.com>
2023-09-13 23:10:56 +0300

    adding create and delete function

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.bookstore.model.Book;
 import com.example.bookstore.model.BookRepository;
 
 @Controller
@@ -19,5 +22,20 @@ public class BookController {
 	    model.addAttribute("books", repository.findAll());
 	    return "booklist";
 	}
+	@RequestMapping(value ="/add")
+	public String addBook(Model model) {	
+	    model.addAttribute("book", new Book());
+	    return "addbook";
+	}
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Book book){
+		repository.save(book);
+		return "redirect:booklist";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteStudent(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
 }
-
