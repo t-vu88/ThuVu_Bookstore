5363ac9f5f34c85690119462c216836fc0e91b95
Thu Trang Vu <vuthutrang2811@gmail.com>
2023-09-27 23:13:43 +0300

    adding new methods (Restful services) to return all books and return one book by id ( JSON)

--- src/main/java/com/example/bookstore/web/BookController.java
+++ src/main/java/com/example/bookstore/web/BookController.java
@@ -1,5 +1,8 @@
 package com.example.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,10 +11,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.bookstore.model.Book;
 import com.example.bookstore.model.BookRepository;
 import com.example.bookstore.model.CategoryRepository;
+
 
 @Controller
 public class BookController {
@@ -20,7 +25,7 @@ public class BookController {
 	
 	@Autowired
 	private CategoryRepository categoryRepo;
-	
+	//show all books
 	@RequestMapping(value ="/booklist")
 	public String bookList(Model model) {	
 	    model.addAttribute("books", repository.findAll());
@@ -33,6 +38,18 @@ public class BookController {
 	    model.addAttribute("categories", categoryRepo.findAll());
 	    return "addbook";
 	}
+	
+	// RESTful service to get all students
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    }    
+    
+ // RESTful service to get student by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findStudentRest(@PathVariable("id") Long bookId) {	
+    	return repository.findById(bookId);
+    }       
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(Book book){
 		repository.save(book);
